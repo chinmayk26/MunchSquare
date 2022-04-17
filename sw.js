5c43a5aeff933edf3d88872d3580c75308406400
ryan10projects <rodricksryan10@gmail.com>
2022-04-17 15:35:49 +0530

    messgae

--- sw.js
+++ sw.js
@@ -59,7 +59,7 @@ self.addEventListener('fetch', evt => {
         });
       }).catch(() => {
         if(evt.request.url.indexOf('.html') > -1){
-          return caches.match('/pages/fallback.html');
+          return caches.match('fallback.html');
         } 
       })
     );
