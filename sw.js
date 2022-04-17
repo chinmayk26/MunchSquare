f85c63c221b25484e5b94347c5f90b8b2590607b
ryan10projects <rodricksryan10@gmail.com>
2022-04-17 15:29:41 +0530

    messgae

--- sw.js
+++ sw.js
@@ -1,3 +1,19 @@
+const assets to.cache = [  
+'/button.html',  
+'/homepage.php',     
+     
+]  
+  
+self. addEventListener ('install', function (event) {  
+    event. wait Until (  
+        caches. Open('staticAssetsCache')  
+        then (function (cache) {  
+              return cache Add All (assetsToCache.);  
+        })  
+      );  
+});  
+
+
 self.addEventListener("install", event => {
   console.log("Service Worker installed.");
 });
