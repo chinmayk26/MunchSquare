8e529eace2ca9fdafb9dc28c10ee53385e81a846
ryan10projects <rodricksryan10@gmail.com>
2022-04-17 16:59:21 +0530

    messgae

--- homepage.php
+++ homepage.php
@@ -443,7 +443,7 @@ window.addEventListener('beforeinstallprompt', (event) => {
   // Stash the event so it can be triggered later.
   window.deferredPrompt = event;
   // Remove the 'hidden' class from the install button container.
-  divInstall.classList.toggle('hidden', false);
+ // divInstall.classList.toggle('hidden', false);
 });
 </script>
   <link rel="manifest" href="./manifest.json">
