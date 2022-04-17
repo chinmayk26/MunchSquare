45884de7383bc512885d3ecffebb6d66bb7071d8
ryan10projects <rodricksryan10@gmail.com>
2022-04-17 15:20:19 +0530

    messgae

--- sw.js
+++ sw.js
@@ -46,3 +46,15 @@ async function networkAndCache(req) {
   }
 }
 
+// install event
+self.addEventListener('install', evt => {
+console.log('service worker installed');
+});
+// activate event
+self.addEventListener('activate', evt => {
+console.log('service worker activated');
+});
+// fetch event
+self.addEventListener('fetch',evt =>{
+console.log(evt.request.fetch);
+});
