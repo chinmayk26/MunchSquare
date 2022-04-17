74587c856049685dc31efaa184e421d057b0e93a
ryan10projects <rodricksryan10@gmail.com>
2022-04-17 17:10:53 +0530

    messgae

--- sw.js
+++ sw.js
@@ -1,13 +1,3 @@
-self.addEventListener('install', evt => {
-  console.log('service worker installed');
-  
-	
-});
-
-self.addEventListener('activate', evt => {
-  console.log('service worker activated');
-});
-
 const staticCacheName = 'site-static-v3';
 const dynamicCacheName = 'site-dynamic-v3';
 const assets = [
