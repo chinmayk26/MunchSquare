2e4962be91dd6987b3acca4dab26008a4111ab86
ryan10projects <rodricksryan10@gmail.com>
2022-04-17 16:22:12 +0530

    messgae

--- homepage.php
+++ homepage.php
@@ -4,9 +4,8 @@
 
 <head>
 <meta charset="UTF-8">
- <link rel="apple-touch-icon" href="images/logo 2.png">
  <meta name="theme-color" content="#4A90E2"/>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <meta name="description" content="hello">
   <link rel="manifest" href="./manifest.json">
