b093aaa39d1eb0a8aad8b4ee37f1b41842c6e866
chinmayk26 <korgaonkarchinmay2659@gmail.com>
2022-05-04 20:17:00 +0530

    Initial Commit

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('sw.js')
   .then(function(registration) {
     console.log('Registration successful, scope is:', registration.scope);
-	navigator.serviceWorker.ready.then(function(swRegistration) {
-    return swRegistration.sync.register('myFirstSync');
   })
   .catch(function(error) {
     console.log('Service worker registration failed, error:', error);
